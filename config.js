18e5e9ff220e39a19fd67a934917b7ca9915c055
Pedro Pablo Velasco <pedropablovelasco@uc.cl>
2025-10-29 15:53:28 -0300

    feat: Add ArUco detector page and update configuration for vehicle detection

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 Protobject.setProduction(false);
 Protobject.initialize([
-  { name: "Aruco NS", page: "aruco_ns.html", debug: "local" },
-  { name: "Aruco EW", page: "aruco_ew.html", debug: "local" },
+  // { name: "Aruco NS", page: "aruco_ns.html", debug: "local" },
+  // { name: "Aruco EW", page: "aruco_ew.html", debug: "local" },
+  { name: "Aruco detector", page: "aruco_detector.html", debug: "local" },
   { name: "Lamp",     page: "index.html",    main: true,    debug: "master" },
   { name: "Remote",   page: "remote.html",   debug: "local" }
 ]);
